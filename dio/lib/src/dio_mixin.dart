629bbb23b9074f979a479a74b47007debfc58b78
Crazelu <eberelucky64@gmail.com>
2021-08-02 02:52:46 +0100

    Ensures data is not null

--- dio/lib/src/dio_mixin.dart
+++ dio/lib/src/dio_mixin.dart
@@ -684,85 +684,87 @@ abstract class DioMixin implements Dio {
     List<int> bytes;
     Stream<List<int>> stream;
     // const allowPayloadMethods = ['POST', 'PUT', 'PATCH', 'DELETE', 'GET'];
-    // if (data != null && allowPayloadMethods.contains(options.method)) {
-    // Handle the FormData
-    int? length;
-    if (data is Stream) {
-      assert(data is Stream<List>,
-          'Stream type must be `Stream<List>`, but ${data.runtimeType} is found.');
-      stream = data as Stream<List<int>>;
-      options.headers.keys.any((String key) {
-        if (key.toLowerCase() == Headers.contentLengthHeader) {
-          length = int.parse(options.headers[key].toString());
-          return true;
-        }
-        return false;
-      });
-    } else if (data is FormData) {
-      options.headers[Headers.contentTypeHeader] =
-          'multipart/form-data; boundary=${data.boundary}';
+    // && allowPayloadMethods.contains(options.method)
+    if (data != null) {
+      // Handle the FormData
+      int? length;
+      if (data is Stream) {
+        assert(data is Stream<List>,
+            'Stream type must be `Stream<List>`, but ${data.runtimeType} is found.');
+        stream = data as Stream<List<int>>;
+        options.headers.keys.any((String key) {
+          if (key.toLowerCase() == Headers.contentLengthHeader) {
+            length = int.parse(options.headers[key].toString());
+            return true;
+          }
+          return false;
+        });
+      } else if (data is FormData) {
+        options.headers[Headers.contentTypeHeader] =
+            'multipart/form-data; boundary=${data.boundary}';
 
-      stream = data.finalize();
-      length = data.length;
-      options.headers[Headers.contentLengthHeader] = length.toString();
-    } else {
-      // Call request transformer.
-      var _data = await transformer.transformRequest(options);
-
-      if (options.requestEncoder != null) {
-        bytes = options.requestEncoder!(_data, options);
+        stream = data.finalize();
+        length = data.length;
+        options.headers[Headers.contentLengthHeader] = length.toString();
       } else {
-        //Default convert to utf8
-        bytes = utf8.encode(_data);
-      }
-      // support data sending progress
-      length = bytes.length;
-      options.headers[Headers.contentLengthHeader] = length.toString();
+        // Call request transformer.
+        var _data = await transformer.transformRequest(options);
 
-      var group = <List<int>>[];
-      const size = 1024;
-      var groupCount = (bytes.length / size).ceil();
-      for (var i = 0; i < groupCount; ++i) {
-        var start = i * size;
-        group.add(bytes.sublist(start, math.min(start + size, bytes.length)));
-      }
-      stream = Stream.fromIterable(group);
-    }
-
-    var complete = 0;
-    var byteStream = stream.transform<Uint8List>(StreamTransformer.fromHandlers(
-      handleData: (data, sink) {
-        final cancelToken = options.cancelToken;
-        if (cancelToken != null && cancelToken.isCancelled) {
-          cancelToken.requestOptions = options;
-          sink
-            ..addError(cancelToken.cancelError!)
-            ..close();
+        if (options.requestEncoder != null) {
+          bytes = options.requestEncoder!(_data, options);
         } else {
-          sink.add(Uint8List.fromList(data));
-          if (length != null) {
-            complete += data.length;
-            if (options.onSendProgress != null) {
-              options.onSendProgress!(complete, length!);
+          //Default convert to utf8
+          bytes = utf8.encode(_data);
+        }
+        // support data sending progress
+        length = bytes.length;
+        options.headers[Headers.contentLengthHeader] = length.toString();
+
+        var group = <List<int>>[];
+        const size = 1024;
+        var groupCount = (bytes.length / size).ceil();
+        for (var i = 0; i < groupCount; ++i) {
+          var start = i * size;
+          group.add(bytes.sublist(start, math.min(start + size, bytes.length)));
+        }
+        stream = Stream.fromIterable(group);
+      }
+
+      var complete = 0;
+      var byteStream =
+          stream.transform<Uint8List>(StreamTransformer.fromHandlers(
+        handleData: (data, sink) {
+          final cancelToken = options.cancelToken;
+          if (cancelToken != null && cancelToken.isCancelled) {
+            cancelToken.requestOptions = options;
+            sink
+              ..addError(cancelToken.cancelError!)
+              ..close();
+          } else {
+            sink.add(Uint8List.fromList(data));
+            if (length != null) {
+              complete += data.length;
+              if (options.onSendProgress != null) {
+                options.onSendProgress!(complete, length!);
+              }
             }
           }
-        }
-      },
-    ));
-    if (options.sendTimeout > 0) {
-      byteStream.timeout(Duration(milliseconds: options.sendTimeout),
-          onTimeout: (sink) {
-        sink.addError(DioError(
-          requestOptions: options,
-          error: 'Sending timeout[${options.connectTimeout}ms]',
-          type: DioErrorType.sendTimeout,
-        ));
-        sink.close();
-      });
+        },
+      ));
+      if (options.sendTimeout > 0) {
+        byteStream.timeout(Duration(milliseconds: options.sendTimeout),
+            onTimeout: (sink) {
+          sink.addError(DioError(
+            requestOptions: options,
+            error: 'Sending timeout[${options.connectTimeout}ms]',
+            type: DioErrorType.sendTimeout,
+          ));
+          sink.close();
+        });
+      }
+      return byteStream;
     }
-    return byteStream;
-    // }
-    // return null;
+    return null;
   }
 
   // If the request has been cancelled, stop request and throw error.
